5f146f2cea7a00922044c2fc093e048b5dc2ffcf
Mattias Petter Johansson <mpj@spotify.com>
2015-08-30 20:17:52 +0200

    looking good

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-export default (facade, input) => {
-  facade('connect', { host: '123.231.121.111', port: 1234 })
+export default (input) => {
+  let [type, opts] = input.message
+
+  switch(type) {
+    case 'connect':
+      return {
+        messages: [
+          [ 'net-connect', { host: opts.host, port: opts.port } ],
+          [ 'net-set-encoding', 'utf8' ]
+        ],
+        state: {
+          connecting: true
+        }
+      }
+    case 'connected':
+      return {
+        state: {
+          connected: true
+        }
+      }
+    case 'play':
+      return {
+        messages: [
+          ['net-write', 'consume\n']
+        ],
+        state: {
+          connected: true,
+          playRequested: true
+        }
+      }
+  }
+
 }
--- test/main.js
+++ test/main.js
@@ -1,17 +1,71 @@
-import main from '../src/main'
-import sinon from 'sinon'
-
+import sharkey from '../src/main'
 const logger = console.log.bind(console, '[LOGGER]')
 
 export default (tape) => {
 
   tape('when passing in connection parameters', (t) => {
     t.plan(1)
-    const facade = sinon.stub()
 
-    main(facade, { host: '123.231.121.111', port: 1234})
-    t.ok(facade.calledWith('connect', {host: "123.231.121.111", port: 1234}),
-      'sends connect command')
+    t.deepLooseEqual(
+      sharkey({
+        state: {},
+        message: ['connect', { host: '123.231.121.111', port: 1234}]
+      }),
+      {
+        state: { connecting: true },
+        messages: [
+          [ 'net-connect', { host: '123.231.121.111', port: 1234 } ],
+          [ 'net-set-encoding', 'utf8' ],
+        ]
+      }
+    )
+
   })
+
+  tape('when passing in connected', (t) => {
+    t.plan(1)
+
+    t.deepEqual(sharkey({
+      message: ['connected'],
+      state: {
+        connecting: true
+      }
+    }),
+    { state: { connected: true } })
+
+  })
+
+  tape('when passing in connected', (t) => {
+    t.plan(1)
+
+    t.deepLooseEqual(sharkey({
+      state: { connecting: true },
+      message: ['connected']
+    }),{
+      state: {
+        connected: true
+      }
+    })
+
+  })
+
+  tape('when issuing play', (t) => {
+    t.plan(1)
+
+    t.deepLooseEqual(sharkey({
+      state: { connected: true },
+      message: [ 'play' ]
+    }), {
+      messages: [
+        [ 'net-write', 'consume\n' ]
+      ],
+      state: {
+        connected: true,
+        playRequested: true
+      }
+    })
+
+  })
+
 
 }
